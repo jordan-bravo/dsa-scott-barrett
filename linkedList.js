4fe5d7d26dc03b9a38e00c19a5de72f1e26755bb
Jordan Brookman <Jordan.Brookman@ibm.com>
2022-03-17 21:38:01 -0400

    Complete Linked List

--- linkedList.js
+++ linkedList.js
@@ -82,9 +82,44 @@ class LinkedList {
 		}
 		return false;
 	}
+	insert(index, value) {
+		if (index === 0) return this.unshift(value);
+		if (index === this.length) return this.push(value);
+		if (index < 0 || index >= this.length) return false;
+		const newNode = new Node(value);
+		const temp = this.get(index - 1);
+		newNode.next = temp.next;
+		temp.next = newNode;
+		this.length++;
+		return true;
+	}
+	remove(index) {
+		if (index === 0) return this.shift();
+		if (index === this.length - 1) return this.pop();
+		if (index < 0 || index >= this.length) return undefined;
+		const before = this.get(index - 1);
+		const temp = before.next;
+		before.next = temp.next;
+		temp.next = null;
+		this.length--;
+		return temp; 
+	}
+	reverse() {
+		let temp = this.head;
+		this.head = this.tail;
+		this.tail = temp;
+		let next = temp.next;
+		let prev = null;
+		for (let i = 0; i < this.length; i++) {
+			next = temp.next;
+			temp.next = prev;
+			prev = temp;
+			temp = next;
+		}
+		return this;
+	}
 }
 
-let myLinkedList = new LinkedList(11);
+let myLinkedList = new LinkedList(1);
+myLinkedList.push(2);
 myLinkedList.push(3);
-myLinkedList.push(23);
-myLinkedList.push(7);
