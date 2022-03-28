44b49e2c497d4467efe096d834f947d6a00de94f
Jordan Brookman <Jordan.Brookman@ibm.com>
2022-03-27 23:23:09 -0400

    Complete 38 DLL: Set

--- doublyLinkedList.js
+++ doublyLinkedList.js
@@ -84,6 +84,14 @@ class DoublyLinkedList {
 		}
 		return temp
 	}
+	set(index, value) {
+		let temp = this.get(index)
+		if (temp) {
+			temp.value = value
+			return true
+		}
+		return false
+	}
 }
 
 let myDoublyLinkedList = new DoublyLinkedList(0)
