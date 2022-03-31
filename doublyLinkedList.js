f2c5fc48afa457e4c444f6e8254f345a51358003
Jordan Brookman <Jordan.Brookman@ibm.com>
2022-03-30 21:05:25 -0400

    Complete 39 DLL: Insert

--- doublyLinkedList.js
+++ doublyLinkedList.js
@@ -92,9 +92,21 @@ class DoublyLinkedList {
 		}
 		return false
 	}
+	insert(index, value) {
+		if (index === 0) return this.unshift(value)
+		if (index === this.length) return this.push(value)
+		if (index < 0 || index > this.length) return false
+		const newNode = new Node(value)
+		const before = this.get(index - 1)
+		const after = before.next
+		before.next = newNode
+		newNode.prev = before
+		newNode.next = after
+		after.prev = newNode
+		this.length++
+		return true
+	}
 }
 
-let myDoublyLinkedList = new DoublyLinkedList(0)
-myDoublyLinkedList.push(1)
-myDoublyLinkedList.push(2)
+let myDoublyLinkedList = new DoublyLinkedList(1)
 myDoublyLinkedList.push(3)
