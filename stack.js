b21fa6500e50738f7fcf396fb3d240a79ee780e6
Jordan Brookman <Jordan.Brookman@ibm.com>
2022-03-30 21:44:00 -0400

    Complete 42 Stack: Constructor

---
+++ stack.js
@@ -0,0 +1,18 @@
+// Stacks
+
+class Node {
+	constructor(value) {
+		this.value = value
+		this.next = null
+	}
+}
+
+class Stack {
+	constructor(value) {
+		const newNode = new Node(value)
+		this.top = newNode
+		this.length = 1
+	}
+}
+
+
