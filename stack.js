4b762120b3b02ce79c69498174a1013ac82a9758
Jordan Brookman <Jordan.Brookman@ibm.com>
2022-04-01 11:49:51 -0400

    Complete 44 Stack Pop

--- stack.js
+++ stack.js
@@ -13,6 +13,28 @@ class Stack {
 		this.top = newNode
 		this.length = 1
 	}
+	push(value) {
+		const newNode = new Node(value)
+		if (this.length === 0) {
+			this.top = newNode
+		} else {
+		newNode.next = this.top
+		this.top = newNode
+		}
+		this.length++
+		return this
+	}
+	pop() {
+		if (this.length === 0) return undefined
+		let temp = this.top
+		this.top = this.top.next
+		temp.next = null
+		this.length--
+		return temp
+	}
 }
 
-
+let myStack = new Stack(7)
+myStack.push(23)
+myStack.push(3)
+myStack.push(11)
