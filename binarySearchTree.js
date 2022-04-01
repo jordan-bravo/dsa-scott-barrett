e9a686ef7f3bfa23389f1e7b57e7e086cb48654e
Jordan Brookman <Jordan.Brookman@ibm.com>
2022-04-01 12:43:35 -0400

    Complete 52 BST: Constructor

---
+++ binarySearchTree.js
@@ -0,0 +1,15 @@
+class Node {
+	constructor(value) {
+		this.value = value
+		this.left = null
+		this.right = null
+	}
+}
+
+class BST {
+	constructor() {
+		this.root = null
+	}
+}
+
+let myTree = new BST()
