3a98c6c255f18ac546f29e9aa2e73ea87bd4a5e5
Jordan Brookman <Jordan.Brookman@ibm.com>
2022-04-05 19:19:58 -0600

    Complete 54 BST: Insert Code

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -12,21 +12,35 @@ class BST {
 	}
 	insert(value) {
 		const newNode = new Node(value)
-		if (this.root = null) {
+		if (this.root === null) {
 			this.root = newNode
 			return this
 		}
 		let temp = this.root
 		while(true) {
 			if (newNode.value === temp.value) return undefined	
-		}
-		if (temp.value < newNode.value) {
-			// go left
-		} else {
-			// go right
-		}
-		
+			if (newNode.value < temp.value) {
+				if (temp.left === null) {
+					temp.left = newNode
+					return this
+				}
+				temp = temp.left	
+			} else {
+				if (temp.right === null) {
+					temp.right = newNode
+					return this
+				}
+				temp = temp.right
+			}
+		}	
 	}
 }
 
 let myTree = new BST()
+myTree.insert(47)
+myTree.insert(21)
+myTree.insert(76)
+myTree.insert(18)
+
+myTree.insert(52)
+myTree.insert(82)
