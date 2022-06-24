c212572a6caa0c82f2e4c689626cfc2b8b92b8c8
Jordan Brookman <Jordan.Brookman@ibm.com>
2022-06-24 17:13:15 -0400

    Complete 59 HT: Constructor

--- hashTable.js
+++ hashTable.js
@@ -2,4 +2,14 @@ class HashTable {
 	constructor(size = 7) {
 		this.dataMap = new Array(size)
 	}
+	_hash(key) {
+		let hash = 0
+		for (let i = 0; i < key; i++) {
+			hash = (hash + key.charCodeAt(i) * 23) % this.dataMap.length
+		}
+		return hash
+	}
 }
+
+let myHashTable = new HashTable()
+myHashTable
