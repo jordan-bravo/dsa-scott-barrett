11da4cfb929fa61c3fd42551d29cd9b4e1a1e5b7
Jordan Brookman <Jordan.Brookman@ibm.com>
2022-07-29 18:16:29 -0700

    Complete 60 HT Set

--- hashTable.js
+++ hashTable.js
@@ -4,10 +4,18 @@ class HashTable {
 	}
 	_hash(key) {
 		let hash = 0
-		for (let i = 0; i < key; i++) {
+		for (let i = 0; i < key.length; i++) {
 			hash = (hash + key.charCodeAt(i) * 23) % this.dataMap.length
 		}
 		return hash
+	}
+	set(key, value) {
+		let index = this._hash(key)
+		if (!this.dataMap[index]) {
+			this.dataMap[index] = []
+		}
+		this.dataMap[index].push([key, value])
+		return this
 	}
 }
 
