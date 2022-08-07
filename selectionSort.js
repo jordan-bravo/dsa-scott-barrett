1b87307b6d3952a503e9a1b8443cd240e352d517
Jordan Brookman <Jordan.Brookman@ibm.com>
2022-08-06 17:54:10 -0700

    Begin 78: Selection Sort

---
+++ selectionSort.js
@@ -0,0 +1,9 @@
+function selectionSort(array) {
+	let min = 0
+	for (let i = 0; i < array.length; i++) {
+		if (array[i ])
+			// let min = i
+			// if array[i] < array[i+1] 
+			// then min = i+1
+	}
+}
