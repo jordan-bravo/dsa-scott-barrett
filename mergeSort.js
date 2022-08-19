895626c755d645068d334942e29dcffb4bab4fd3
Jordan Brookman <Jordan.Brookman@ibm.com>
2022-08-19 10:55:30 -0700

    Complete mergeSort

--- mergeSort.js
+++ mergeSort.js
@@ -1,40 +1,34 @@
-function mergeSort(array) {
-	if (array.length === 1) merge(array1, array2)
-
-	let mid = Math.floor(array.length/2)
-	let left = array.slice(0, mid)
-	let right = array.slice(mid)
-
-	return merge(left, right )
-	function merge(array1, array2) {
-		let combined = []	
-		let i = 0
-		let j = 0
-		while (i < array1.length && j < array2.length) {
-			if (array1[i] < array2[j]) {
-				combined.push(array1[i])
-				i++
-			} else {
-				combined.push(array2[j])
-				j++
-			}
-		}
-		while (i < array1.length) {
+function merge(array1, array2) {
+	let combined = []	
+	let i = 0
+	let j = 0
+	while (i < array1.length && j < array2.length) {
+		if (array1[i] < array2[j]) {
 			combined.push(array1[i])
 			i++
-		}
-		while (j < array2.length) {
+		} else {
 			combined.push(array2[j])
 			j++
 		}
-		return combined
 	}
-	let array1 = []
-	let array2 = []
-	for (let m = 0; m < array.length / 2; m++) {
-		array1.push(array[m])
+	while (i < array1.length) {
+		combined.push(array1[i])
+		i++
 	}
-	for (let n = array.length / 2; n < array.length; n++) {
-		array2.push(array[n])
+	while (j < array2.length) {
+		combined.push(array2[j])
+		j++
 	}
+	return combined
 }
+function mergeSort(array) {
+	if (array.length === 1) return array
+
+	let halfLength = Math.floor(array.length/2)
+	let leftArray = array.slice(0, halfLength)
+	let rightArray = array.slice(halfLength)
+
+	return merge( mergeSort(leftArray), mergeSort(rightArray) )
+}
+
+mergeSort([3,1,4,2])
